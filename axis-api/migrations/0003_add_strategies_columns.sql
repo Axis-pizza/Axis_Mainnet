557d84e869b4234d997e54f3197495f4b258a598
ゆう <y.shibata0820@gmail.com>
2026-03-09 14:16:04 +0900

    migration: kagemusha.tsを元にschema.tsとdbを修正

---
+++ axis-api/migrations/0003_add_strategies_columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `strategies` ADD `composition` text;
+ALTER TABLE `strategies` ADD `tvl` real;
+ALTER TABLE `strategies` ADD `roi` real;
+ALTER TABLE `strategies` ADD `mint_address` text;
+ALTER TABLE `strategies` ADD `vault_address` text;
